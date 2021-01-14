49a009338a2d5479f8d798caa3ba97e563a4030d
R S JALDS <R S JALDS@DESKTOP-94V18RE>
2021-01-14 15:21:33 +0530

    Add project files.

---
+++ Rah1stProj/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Rah1stProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           string a=Console.ReadLine();
+            Console.WriteLine(a);
+            int c=int.Parse(a);
+            c += 5;
+            Console.WriteLine(c);
+        }
+    }
+}
